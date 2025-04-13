d736843f3c4789c3385ec29de530f8ad4f0db761
Bogdan Romenskii <rmnsk@seznam.cz>
2025-04-13 16:40:32 +0200

    feat: Integrate MongoDB with Spark

--- scripts/mongodb/config.py
+++ scripts/mongodb/config.py
@@ -1,5 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 def get_conn_uri(db: str, collection: str) -> str:
+    user = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+    pwd = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
     host = "mongodb"
     port = 27017
 
-    return f"mongodb://{host}:{port}/{db}:{collection}"
+    return f"mongodb://{user}:{pwd}@{host}:{port}/{db}:{collection}?authSource=admin"
+
+
+if __name__ == "__main__":
+    print(get_conn_uri(db='shakespeare', collection='words'))
