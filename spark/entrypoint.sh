8cf72e78957dacc3f8bc5183a574d0c7ba5a83a3
Bogdan Romenskii <rmnsk@seznam.cz>
2025-04-15 02:29:55 +0200

    fix: Resolve the Docker spin up Spark bugs

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -4,8 +4,13 @@ SPARK_WORKLOAD=$1
 
 echo "SPARK_WORKLOAD: $SPARK_WORKLOAD"
 
-if [ "$SPARK_WORKLOAD" == "master" ]; then
+if [ "$SPARK_WORKLOAD" == "master" ];
+then
   start-master.sh -p 7077
-elif [ "$SPARK_WORKLOAD" == "worker" ]; then
+elif [ "$SPARK_WORKLOAD" == "worker" ];
+then
   start-worker.sh spark://spark-master:7077
+elif [ "$SPARK_WORKLOAD" == "history" ]
+then
+  start-history-server.sh
 fi
