214e23b0d9e1f0e8acfa267ccf724487a014f324
Bogdan Romenskii <rmnsk@seznam.cz>
2025-04-18 21:27:20 +0200

    feat: Add reusable Spark constructor

--- scripts/utils/etl/base.py
+++ scripts/utils/etl/base.py
@@ -1,12 +1,40 @@
-from pathlib import Path
-from typing import TypeAlias
+from typing import TypeAlias, Optional
 
-from pyspark import RDD
-from pyspark.sql import DataFrame
+from pyspark import RDD, SparkConf, SparkContext
+from pyspark.sql import SparkSession, DataFrame
 
 SparkDataAbstract: TypeAlias = RDD | DataFrame
 
 
-class DataETL:
-    cwd = Path.cwd()
-    data_folder = cwd.parent.joinpath('data')
+class SparkBase:
+
+    def __init__(
+            self
+            , conn: Optional[str] = ''
+            , sc_name: Optional[str] = 'generic-sc-app-name'
+            , spark_name: Optional[str] = 'generic-spark-app-name'
+    ):
+        # Update the Spark Config with Connection URI programmatically
+        self._conf = (
+            SparkConf()
+            .set('spark.mongodb.read.connection.uri', conn)
+            .set('spark.mongodb.write.connection.uri', conn)
+        )
+
+        self._sc = SparkContext(appName=sc_name, conf=self._conf)
+
+        self._spark = (
+            SparkSession
+            .builder
+            .appName(spark_name)
+            .config(conf=self._conf)
+            .getOrCreate()
+        )
+
+    @property
+    def sc(self) -> SparkContext:
+        return self._sc
+
+    @property
+    def spark(self) -> SparkSession:
+        return self._spark
