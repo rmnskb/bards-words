20973e870c8be4389c4a758420cc18046fa6cdce
Bogdan Romenskii <rmnsk@seznam.cz>
2025-04-18 21:28:01 +0200

    feat: Add containers for Spark schemas

---
+++ scripts/utils/etl/schemas.py
@@ -0,0 +1,52 @@
+from typing import Any
+
+from pyspark.sql.types import StructType, StructField, ArrayType, StringType, IntegerType
+
+
+class SparkSchema:
+    _schema: StructType
+
+    @classmethod
+    def get(cls):
+        return cls._schema
+
+    @classmethod
+    def to_dict(cls) -> dict[str, Any]:
+        return cls._schema.jsonValue()
+
+    @classmethod
+    def equals(cls, other: StructType) -> bool:
+        # Faulty implementation atm, since inferred schemas are different
+        # in their metadata, as opposed to manually created
+        return cls.to_dict() == other.jsonValue()
+
+
+class TokensSchema(SparkSchema):
+    _schema = StructType([
+        StructField('document', StringType())
+        , StructField('occurrences', ArrayType(StringType()))
+    ])
+
+
+class InvertedIndexSchema(SparkSchema):
+    _schema = StructType([
+        StructField('word', StringType(), False)
+        , StructField('occurrences', ArrayType(
+            StructType([
+                StructField('document', StringType(), False)
+                , StructField('frequency', IntegerType(), False)
+                , StructField('indices', ArrayType(
+                    IntegerType(), False
+                ), False)
+            ])
+        ), False)
+    ])
+
+
+class NormalisedIndexSchema(SparkSchema):
+    _schema = StructType([
+        StructField('word', StringType(), True)
+        , StructField('document', StringType(), True)
+        , StructField('frequency', IntegerType(), True)
+        , StructField('positionIdx', IntegerType(), True)
+    ])
