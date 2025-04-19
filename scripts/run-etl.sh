aa7e9855c12f74f1841b459769bb2d375439e0c9
Bogdan Romenskii <rmnsk@seznam.cz>
2025-04-20 00:11:41 +0200

    refactor: Unify the submission bash scripts

--- scripts/run-etl.sh
+++ scripts/run-etl.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+build_dependencies() {
+  # Takes the dependencies directory as its only argument,
+  # creates .zip file with the same name
+  local directory=$1
+
+  echo "Building dependencies from following directory: $directory"
+
+  test -d "$directory" || (echo "The Spark dependencies directory does not exist" && exit)
+
+  test -f ./"$directory".zip && echo "Deleting previous build..." && rm ./"$directory".zip
+
+  zip -r ./"$directory".zip "$directory" && echo "Dependency built successfully"
+}
+
+
 layer=$1
+directory=${2:-"utils"}  # Either take the argument provided by user or put a default one
 
-test -f build-dependencies.sh && sh build-dependencies.sh utils
+build_dependencies "$directory"
 
-spark-submit --py-files utils.zip "${layer}_etl.py"
+spark-submit --py-files "${directory}.zip" "${layer}_etl.py"
