2b6d73da034be3442d9c93d902a3c4f4038656c5
Bogdan Romenskii <rmnsk@seznam.cz>
2025-04-20 00:09:50 +0200

    upd: Separate user accesses for MongoDB

--- scripts/utils/mongodb/config.py
+++ scripts/utils/mongodb/config.py
@@ -4,14 +4,14 @@ import os
 # load_dotenv()
 
 
-def get_conn_uri(db: str, collection: str) -> str:
+def get_etl_conn_uri() -> str:
     user = os.getenv("MONGO_INITDB_ROOT_USERNAME")
     pwd = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
     host = "mongodb"
     port = 27017
 
-    return f"mongodb://{user}:{pwd}@{host}:{port}/{db}:{collection}?authSource=admin"
+    return f"mongodb://{user}:{pwd}@{host}:{port}/shakespeare?authSource=shakespeare"
 
 
 if __name__ == "__main__":
-    print(get_conn_uri(db='shakespeare', collection='words'))
+    print(get_etl_conn_uri())
