64a7c6b5a87bc12a828ceaf5feb5d55e8e9a6d53
Bogdan Romenskii <rmnsk@seznam.cz>
2025-04-20 03:44:56 +0200

    feat: Initialise a baseline repo for interacting with MongoDB

---
+++ api/mongodb/queries.py
@@ -0,0 +1,48 @@
+import os
+import re
+
+from pymongo import AsyncMongoClient
+
+from .models import InvertedIndexItem
+
+
+class _MongoRepository:
+
+    def __init__(self):
+        uri = self._get_conn_uri()
+        self._client = AsyncMongoClient(uri)
+
+    @staticmethod
+    def _get_conn_uri():
+        user = os.getenv("DB_API_USER")
+        pwd = os.getenv("DB_API_PWD")
+        host = "mongodb"
+        port = 27017
+
+        return f"mongodb://{user}:{pwd}@{host}:{port}/shakespeare?authSource=shakespeare"
+
+    @property
+    def client(self) -> AsyncMongoClient:
+        return self._client
+
+
+class ShakespeareRepository(_MongoRepository):
+
+    def __init__(self):
+        super().__init__()
+        self._db = self.client['shakespeare']
+
+    async def find_word(self, word: str) -> InvertedIndexItem:
+        """
+        Return the best match for the given word,
+        works like SQL LIKE %word% query
+
+        :param word: the search criteria
+        :return: BSON match output
+        """
+        regexp = re.compile(rf".*{word}.*", re.IGNORECASE)
+
+        result = await self._db.bronzeIndices.find_one({"word": regexp})
+        item = InvertedIndexItem(**result)
+
+        return item
