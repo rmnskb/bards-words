f3fc8f519faf4a90dbdb261feecec82ba5df545c
Bogdan Romenskii <rmnsk@seznam.cz>
2025-04-20 03:44:11 +0200

    feat: Add data models for incoming query results

---
+++ api/mongodb/models.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+from bson import ObjectId
+
+
+class MongoQueryResult(BaseModel):
+    id: ObjectId = Field(None, exclude=True, alias="_id")
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+class _OccurrenceElement(BaseModel):
+    document: str
+    frequency: int
+    indices: list[int]
+
+
+class InvertedIndexItem(MongoQueryResult):
+    word: str
+    occurrences: list[_OccurrenceElement]
+
+
+class NormalisedIndexItem(MongoQueryResult):
+    word: str
+    document: str
+    frequency: int
+    positionIdx: int
