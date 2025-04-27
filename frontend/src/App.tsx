ae9bb3267ab85a233b24bdc42658028f674480e5
Bogdan Romenskii <rmnsk@seznam.cz>
2025-04-27 18:11:06 +0200

    chore: Ensure that React container is spun up in dev mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+            </div>
+        </>
+    )
 }
 
 export default App
