f211153ffaadcd7a104713e208081b0d9ffafdd8
Bogdan Romenskii <rmnsk@seznam.cz>
2025-05-01 17:47:09 +0200

    feat: Initialise Words route

---
+++ frontend/src/words/WordPage.tsx
@@ -0,0 +1,18 @@
+// import {useState} from "react";
+// import {useParams} from "react-router";
+//
+// import IWordIndex from "../WordIndex.ts";
+
+const WordPage = () => {
+    // const [loading, setLoading] = useState<boolean>(false);
+    // const [wordIndex, setWordIndex]: IWordIndex = useState<IWordIndex | null>(null);
+    // const params = useParams();
+    // const word: string = String(params.word);
+
+    return (
+        <>
+        </>
+    )
+}
+
+export default WordPage;
