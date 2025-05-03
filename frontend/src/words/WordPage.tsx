1cd7750cfac279515c7958362526c5ba08676901
Bogdan Romenskii <rmnsk@seznam.cz>
2025-05-03 17:38:12 +0200

    feat: Populate the Word-related page w/ basic content

--- frontend/src/words/WordPage.tsx
+++ frontend/src/words/WordPage.tsx
@@ -1,16 +1,71 @@
-// import {useState} from "react";
-// import {useParams} from "react-router";
-//
-// import IWordIndex from "../WordIndex.ts";
+import {useState, useEffect} from "react";
+import {useParams} from "react-router";
+import axios, {AxiosResponse} from "axios";
+
+import {IWordDimensions, YearFrequencyElement} from "../WordInterfaces.ts";
 
 const WordPage = () => {
-    // const [loading, setLoading] = useState<boolean>(false);
-    // const [wordIndex, setWordIndex]: IWordIndex = useState<IWordIndex | null>(null);
-    // const params = useParams();
-    // const word: string = String(params.word);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
+    const [wordDimensions, setWordDimensions] = useState<IWordDimensions | null>(null);
+    const params = useParams();
+    const word = String(params.word);
+    const apiUrl = "//localhost:8000/api/v1"
+
+    const fetchWord = async (word: string): Promise<IWordDimensions | null> => {
+        try {
+            const response: AxiosResponse<IWordDimensions> = await axios.get(`${apiUrl}/get-stats?word=${word}`);
+            return response.data;
+        } catch (errorMsg) {
+            console.error('Error fetching search', errorMsg);
+            return null;
+        }
+    };
+
+    useEffect(() => {
+        setLoading(true);
+        fetchWord(word)
+            .then((response) => {
+                setWordDimensions(response);
+                setLoading(false);
+            })
+            .catch((e: string) => {
+                setError(e);
+                setLoading(false);
+            })
+    }, [word]);
+
+    const findFirstAppearance
+        = (stats: YearFrequencyElement[]): number | undefined => {
+        if (stats.length === 0) {
+            return undefined;
+        }
+
+        return Math.min(...stats.map(entry => entry.year));
+    };
+
+    const calculateTotalFrequency
+        = (stats: YearFrequencyElement[]): number | undefined => {
+        if (stats.length === 0) {
+            return undefined;
+        }
+
+        return stats.map(entry => entry.frequency).reduce((x, y) => x + y, 0);
+    };
 
     return (
         <>
+            {loading && (<p>Loading...</p>)}
+            {error && (<p>{error}</p>)}
+            {
+                wordDimensions && (
+                    <div>
+                        <h1>{wordDimensions.word}</h1>
+                        <p>First Appearance: {findFirstAppearance(wordDimensions.yearFrequencies)}</p>
+                        <p>Total Frequency: {calculateTotalFrequency(wordDimensions.yearFrequencies)}</p>
+                    </div>
+                )
+            }
         </>
     )
 }
