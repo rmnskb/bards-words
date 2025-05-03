8fff89e3bbdca37a10f97088e117764b32b478b6
Bogdan Romenskii <rmnsk@seznam.cz>
2025-05-03 17:33:41 +0200

    chore: Initialise baseline routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
+import {BrowserRouter, Routes, Route} from "react-router";
+
 import HomePage from "./home/HomePage.tsx";
+import WordPage from "./words/WordPage.tsx";
 
 function App() {
-
     return (
-        <>
-            <HomePage/>
-        </>
-    )
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/words" element={<WordPage/>}></Route>
+                <Route path="/words/:word" element={<WordPage/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
