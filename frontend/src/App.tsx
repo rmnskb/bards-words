7cc1567d5f41150c7ef1d0da8e9959f3f39d66ba
Bogdan Romenskii <rmnsk@seznam.cz>
2025-06-21 23:47:10 +0200

    feat: Add separate Wordle Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,30 +4,29 @@ import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
 import WordPage from "./pages/WordPage";
 import PlayPage from "./pages/PlayPage";
+import WordlePage from "./pages/WordlePage";
 
 /**
  * A list of ideas that can be implemented in future iterations of the application
  * TODO: Word of the day? :D -> Wordle
  */
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" 
-          element={
-            <Layout/>
-          }>
-          <Route index element={<HomePage/>}/>
-            <Route path="/words/:word" element={<WordPage/>}></Route>
-              <Route
-                path="/plays/:document/:indices?"
-                element={<PlayPage/>}
-              ></Route>
-            </Route>
-        </Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/words/:word" element={<WordPage />}></Route>
+          <Route
+            path="/plays/:document/:indices?"
+            element={<PlayPage />}
+          ></Route>
+          <Route path="/wordle" element={<WordlePage />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/pages/WordlePage.tsx
@@ -0,0 +1,9 @@
+const WordlePage = () => {
+  return (
+    <div>
+      <p>Hello on Wordle Page!</p>
+    </div>
+  );
+};
+
+export default WordlePage;
