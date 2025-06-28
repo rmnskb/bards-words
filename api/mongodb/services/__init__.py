2ed35f2549ced73bc3e9732790fca9ff940c37ab
Bogdan Romenskii <rmnsk@seznam.cz>
2025-06-28 21:41:12 +0200

    feat: Add independent Stats service

--- api/mongodb/services/__init__.py
+++ api/mongodb/services/__init__.py
@@ -1,3 +1,4 @@
 from .wordle_service import WordleService
 from .word_service import WordService
 from .tokens_service import TokensService, AdjacentIndicesType
+from .stats_service import StatsService
