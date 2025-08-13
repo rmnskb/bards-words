472103cb0795cb076caea3481973bb84e7c68a26
Bogdan Romenskii <rmnsk@seznam.cz>
2025-08-14 00:08:50 +0200

    feat: Enhance the input validator logic

--- api/utils.py
+++ api/utils.py
@@ -3,10 +3,15 @@ from typing import Any, Awaitable, Callable, Optional, TypeVar
 from fastapi import HTTPException, Query
 
 T = TypeVar('T')
+type ParamInput = str | int | list[Any]
 
 
-def require_param(q: Optional[T] = Query(None)) -> T:
-    if not q or (isinstance(q, str) and not q.strip()):
+def require_param(q: Optional[ParamInput] = Query(None)) -> ParamInput:
+    str_check = (isinstance(q, str) and not q.strip())
+    int_check = (isinstance(q, int) and q < 0)
+    list_check = (isinstance(q, list) and not q)
+
+    if str_check or int_check or list_check or q is None:
         raise HTTPException(status_code=400, detail="Query parameter is required")
 
     return q
