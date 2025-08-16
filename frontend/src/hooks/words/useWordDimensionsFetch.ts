7aa914385491785475b514df4e5346e63424da2f
Bogdan Romenskii <rmnsk@seznam.cz>
2025-08-16 17:35:01 +0200

    refactor: Update the way the stats API is accessed

--- frontend/src/hooks/words/useWordDimensionsFetch.ts
+++ frontend/src/hooks/words/useWordDimensionsFetch.ts
@@ -1,33 +1,55 @@
-import { useState, useEffect } from "react";
 import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react";
 
-import { IWordDimensions } from "../../types";
-import { apiUrl } from "../../constants";
+import { API_URL } from "../../constants";
+import { IDocumentFrequencies, IWordDimensions, IYearFrequencies } from "../../types";
 
 
 const useWordDimensionsFetch = (word: string): IWordDimensions | null => {
   const [wordDimensions, setWordDimensions] = useState<IWordDimensions | null>(null);
+  const [yearFrequencies, setYearFrequencies] = useState<IYearFrequencies | null>(null);
+  const [documentFrequencies, setDocumentFrequencies] = useState<IDocumentFrequencies | null>(null);
 
-  const fetchWordDimensions
-    = async (word: string): Promise<IWordDimensions | null> => {
+  const fetchYearFrequencies 
+    = async (word: string): Promise<IYearFrequencies | null> => {
     try {
-      const response: AxiosResponse<IWordDimensions> = await axios.get(`${apiUrl}/stats?word=${word}`);
+      const response: AxiosResponse<IYearFrequencies> = 
+        await axios.get(`${API_URL}/stats/years?word=${word}`)
       return response.data;
-    } catch (errorMsg) {
-      console.error('Error fetching search', errorMsg);
+    } catch(errorMsg) {
+      console.error('Error fetching year frequencies', errorMsg);
+      return null;
+    }
+  };
+
+  const fetchDocumentFrequencies 
+    = async (word: string): Promise<IDocumentFrequencies | null> => {
+    try {
+      const response: AxiosResponse<IDocumentFrequencies> =
+        await axios.get(`${API_URL}/stats/documents?word=${word}`)
+      return response.data;
+    } catch(errorMsg) {
+      console.error('Error fetching document frequencies', errorMsg);
       return null;
     }
   };
 
   useEffect(() => {
-    fetchWordDimensions(word)
-      .then((response) => {
-        setWordDimensions(response);
-      })
-      .catch((e: string) => {
-        console.error(e);
-      });
+    fetchYearFrequencies(word)
+      .then((response) => setYearFrequencies(response))
+      .catch((e: string) => console.error(e));
+
+    fetchDocumentFrequencies(word)
+      .then((response) => setDocumentFrequencies(response))
+      .catch((e: string) => console.error(e));
   }, [word]);
+
+  useEffect(() => {
+    if (!yearFrequencies || !documentFrequencies) return;
+    if (yearFrequencies.word !== documentFrequencies.word) return;
+
+    setWordDimensions({...yearFrequencies, ...documentFrequencies})
+  }, [yearFrequencies, documentFrequencies])
 
   return wordDimensions;
 };
